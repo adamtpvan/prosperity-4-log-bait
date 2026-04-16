d9b108146f862f470d9be1dda100cb224d3593c8
adamtpvan <adamtrungvan@gmail.com>
2026-04-15 23:08:08 -0400

    added functions that I want coded

--- algorithm.py
+++ algorithm.py
@@ -7,36 +7,34 @@ class Trader:
     def bid(self):
         return 15
     
-    def run(self, state: TradingState):
+    def update_data(self):
+        #update mid price data
+        pass
+
+    def root_fair_price(self):
+        #add calculated slope to last data point
+        pass
+    
+    def take_book(self):
+        #buy/sell the entire side of a book 
+        pass
+
+    def intarian_root_take(self):
+        #check around calculated fair price for favorable trades
+        pass
+
+    def market_make(self, state, mid_value, half_edge, orders):
+        #market make
+        pass
+    
+    def run(self, state: TradingState) -> dict: #dict should be product with value list of orders
         """Only method required. It takes all buy and sell orders for all
         symbols as an input, and outputs a list of orders to be sent."""
 
-        print("traderData: " + state.traderData)
-        print("Observations: " + str(state.observations))
-
         # Orders to be placed on exchange matching engine
-        result = {}
-        for product in state.order_depths:
-            order_depth: OrderDepth = state.order_depths[product]
-            orders: List[Order] = []
-            acceptable_price = 10  # Participant should calculate this value
-            print("Acceptable price : " + str(acceptable_price))
-            print("Buy Order depth : " + str(len(order_depth.buy_orders)) + ", Sell order depth : " + str(len(order_depth.sell_orders)))
-    
-            if len(order_depth.sell_orders) != 0:
-                best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
-                if int(best_ask) < acceptable_price:
-                    print("BUY", str(-best_ask_amount) + "x", best_ask)
-                    orders.append(Order(product, best_ask, -best_ask_amount))
-    
-            if len(order_depth.buy_orders) != 0:
-                best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
-                if int(best_bid) > acceptable_price:
-                    print("SELL", str(best_bid_amount) + "x", best_bid)
-                    orders.append(Order(product, best_bid, -best_bid_amount))
-            
-            result[product] = orders
-    
+        result = {"INTARIAN_PEPPER_ROOT" : [], "ASH_COATED_OSMIUM" : []}
+
+
         traderData = ""  # No state needed - we check position directly
         conversions = 0
         return result, conversions, traderData
