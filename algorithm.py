5d11d67da41e7e3ab19ff0cf8042d408c0ce284d
adamtpvan <adamtrungvan@gmail.com>
2026-04-17 00:07:04 -0400

    working algo for intarian root

--- algorithm.py
+++ algorithm.py
@@ -4,22 +4,16 @@ import string
 import numpy as np
 import jsonpickle
 
-#note t = 0 to t = 1,000,000, ticks are 100 a piece
-
 class Trader:
 
     def bid(self):
         return 15
     
     def root_update_data(self, state : TradingState, historical_data : list):
-        # calculates mid price and updates historical data list 
-
-        # initialize variables
         order_depths = state.order_depths['INTARIAN_PEPPER_ROOT']
         buy_orders = order_depths.buy_orders
         sell_orders = order_depths.sell_orders
 
-        # define best_bid and best_ask if not empty, else use last mid price
         if buy_orders:
             best_bid = max(buy_orders)
         else:
@@ -29,7 +23,6 @@ class Trader:
         else:
             best_ask = 0
 
-        # calculate mid price and append to list 
         if best_bid != 0 and best_ask != 0:
             mid_price = (best_bid + best_ask) / 2
         else:
@@ -40,72 +33,37 @@ class Trader:
         else:
             historical_data.append(mid_price)
         
-
-    def root_fair_price(self, state : TradingState, historical_data : list, historical_fair_price : list) -> float:
-        # calculates fair_price and updates historical fair_price
-
-        # calculate slope given historical data
-        x = np.arrange(len(historical_data))
+    def root_fair_price(self, state : TradingState, historical_data : list, historical_fair_price : float) -> tuple:
+        x = np.arange(len(historical_data))
         slope, intercept = np.polyfit(x, historical_data, 1)
 
-        # calculate historical_fair_price and append
-        if len(historical_fair_price) == 0:
-            historical_fair_price.append(historical_data[-1])
+        if historical_fair_price == 0:
+            historical_fair_price = historical_data[-1]
         else:
-            historical_fair_price.append(historical_fair_price[-1] + slope)
+            historical_fair_price = historical_fair_price + slope
         
-        return slope
+        return slope, historical_fair_price
     
-    def take_book(self, state : TradingState, action : int, product : string, max_position : int, max_half_edge : float, historical_fair_price : list, orders : list[Order]):
-        # buy/sell the entire side of a book 
-        
-        # initialize variables
+    def take_book(self, state : TradingState, action : int, product : string, max_position : int, max_half_edge : float, historical_fair_price : float, orders : list[Order]):
         order_depth = state.order_depths[product]
         buy_orders = order_depth.buy_orders 
         sell_orders = order_depth.sell_orders 
-        position = state.position[product]
+        position = state.position.get(product, 0)
 
-
-        # take orders
-        # want to buy, so take the sell book
         if action == 1:
-            # we want to buy, so take the sell book at a slight premium so +
-            clearing_price = historical_fair_price[-1] + max_half_edge
+            clearing_price = historical_fair_price + max_half_edge
             for value in sell_orders:
                 if value <= clearing_price:
                     orders.append(Order(product, value, min(-sell_orders[value], max_position - position)))
-        # want to sell, so take the buy book
         else:
-            # we want to sell, so take the buy book at a slight premium so -
-            clearing_price = historical_fair_price[-1] - max_half_edge
+            clearing_price = historical_fair_price - max_half_edge
             for value in buy_orders:
-                if value >= sell_orders:
-                    orders.append(Order(product, value, max(-buy_orders[value] , max_position - position)))
-
-    def intarian_root_take(self, state: TradingState, historical_fair_price : list, orders : list[Order]) -> int:
-        #check around calculated fair price for favorable trades
-
-        #initialize variables
-        order_depth = state.order_depths['INTARIAN_PEPPER_ROOT']
-        buy_orders = order_depth.buy_orders 
-        sell_orders = order_depth.sell_orders 
-        position = state.position['INTARIAN_PEPPER_ROOT']
-
-        #fair price
-        fair_price = historical_fair_price[-1]
-
-        for value in sell_orders:
-            if value < fair_price:
-                orders.append(Order("INTARIAN_PEPPER_ROOT", value, min(-sell_orders[value], 80 - position)))
-        for value in buy_orders:
-            if value > fair_price:
-                    orders.append(Order("INTARIAN_PEPPER_ROOT", value, max(-buy_orders[value] , 80 - position)))
+                if value >= clearing_price:
+                    orders.append(Order(product, value, max(-buy_orders[value], max_position - position)))
     
-    def run(self, state: TradingState) -> dict: #dict should be product with value list of orders
-        # initialize variables
-        # traderData is a list of historical_data, historical_fair_price
+    def run(self, state: TradingState) -> dict:
         historical_data = {"INTARIAN_PEPPER_ROOT" : [], "ASH_COATED_OSMIUM" : []}
-        historical_fair_price = {"INTARIAN_PEPPER_ROOT" : [], "ASH_COATED_OSMIUM" : []}
+        historical_fair_price = {"INTARIAN_PEPPER_ROOT" : 0, "ASH_COATED_OSMIUM" : 0}
         hold_indicator = 1
         if state.traderData:
             historical_data, historical_fair_price, hold_indicator = jsonpickle.decode(state.traderData)
@@ -114,22 +72,21 @@ class Trader:
         root_orders = []
         osmium_orders = []
 
-        #allow time for slope
         if time < 5000:
             self.root_update_data(state, historical_data['INTARIAN_PEPPER_ROOT'])
-        #if we havent bought yet, buy now
         elif hold_indicator != -1:
-            #slope going up, buy
-            if self.root_fair_price(state, historical_data['INTARIAN_PEPPER_ROOT'], historical_fair_price['INTARIAN_PEPPER_ROOT']) > 0:
-                self.take_book(state, 1, 'INTARIAN_PEPPER_ROOT', 75, 999999999, historical_fair_price['INTARIAN_PEPPER_ROOT'], root_orders)
-            #slope going down, sell
+            slope, historical_fair_price['INTARIAN_PEPPER_ROOT'] = self.root_fair_price(
+                state,
+                historical_data['INTARIAN_PEPPER_ROOT'],
+                historical_fair_price['INTARIAN_PEPPER_ROOT']
+            )
+            if slope > 0:
+                self.take_book(state, 1, 'INTARIAN_PEPPER_ROOT', 80, 5, historical_fair_price['INTARIAN_PEPPER_ROOT'], root_orders)
             else:
-                self.take_book(state, -1, 'INTARIAN_PEPPER_ROOT', 75, 999999999, historical_fair_price['INTARIAN_PEPPER_ROOT'], root_orders)
-            #this wil change the indicator on the tick after we get a full position but should be okay
-            if abs(positions['INTARIAN_PEPPER_ROOT']) == 75:
+                self.take_book(state, -1, 'INTARIAN_PEPPER_ROOT', 80, 5, historical_fair_price['INTARIAN_PEPPER_ROOT'], root_orders)
+            if abs(positions.get('INTARIAN_PEPPER_ROOT', 0)) == 75:
                hold_indicator = -1
 
-        # Orders to be placed on exchange matching engine
         result = {"INTARIAN_PEPPER_ROOT" : root_orders, "ASH_COATED_OSMIUM" : osmium_orders}
 
         traderData = jsonpickle.encode([historical_data, historical_fair_price, hold_indicator])
