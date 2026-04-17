515a83c8562f5a8cc08c60fe0fb4d5bb9e8e1204
adamtpvan <adamtrungvan@gmail.com>
2026-04-16 22:08:38 -0400

    algo work for root strategy

--- algorithm.py
+++ algorithm.py
@@ -1,29 +1,69 @@
 from datamodel import OrderDepth, UserId, TradingState, Order
 from typing import List
 import string
+import numpy as np
+
+#note t = 0 to t = 1,000,000, ticks are 100 a piece
 
 class Trader:
 
     def bid(self):
         return 15
     
-    def update_data(self):
-        #update mid price data
-        pass
+    def root_update_data(self, state : TradingState, historical_data : list):
+        #calculates mid price and updates historical data list 
 
-    def root_fair_price(self):
-        #add calculated slope to last data point
-        pass
+        #initialize variables
+        order_depths = state.order_depths
+        buy_orders = order_depths.buy_orders
+        sell_orders = order_depths.sell_orders
+
+        #define best_bid and best_ask if not empty, else use last mid price
+        if buy_orders:
+            best_bid = max(buy_orders)
+        else:
+            best_bid = 0
+        if sell_orders:    
+            best_ask = min(sell_orders)
+        else:
+            best_ask = 0
+
+        #calculate mid price and append to list 
+        if best_bid != 0 and best_ask != 0:
+            mid_price = (best_bid + best_ask) / 2
+        else:
+            mid_price = max(best_bid, best_ask)
+
+        if mid_price == 0 and len(historical_data) >= 1:
+            historical_data.append(historical_data[-1])
+        else:
+            historical_data.append(mid_price)
+        
+
+    def root_fair_price(self, state : TradingState, historical_data : list, historical_fair_price : list) -> int:
+        #calculates fair_price and updates historical fair_price
+
+        #calculate slope given historical data
+        x = np.arrange(len(historical_data))
+        slope, intercept = np.polyfit(x, historical_data, 1)
+
+        #calculate historical_fair_price and append
+        if len(historical_fair_price) == 0:
+            historical_fair_price.append(historical_data[-1])
+        else:
+            historical_fair_price.append(historical_fair_price[-1] + slope)
+        
+        return slope
     
-    def take_book(self):
+    def take_book(self, state : TradingState):
         #buy/sell the entire side of a book 
         pass
 
-    def intarian_root_take(self):
+    def intarian_root_take(self, state: TradingState) -> int:
         #check around calculated fair price for favorable trades
         pass
 
-    def market_make(self, state, mid_value, half_edge, orders):
+    def market_make(self, state : TradingState, mid_value : int, half_edge : int, orders : int):
         #market make
         pass
     
